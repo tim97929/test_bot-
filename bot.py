6a786810b3a79734ca527738f1969b3c98c498c8
tim97929 <91124766+tim97929@users.noreply.github.com>
2021-09-21 19:47:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ bot = commands.Bot(command_prefix='[')
 async def on_ready():
    print(">>bot is online<<")
 
-bot.run('ODg5NzU1OTY0MjI2NTQ3NzUy.YUl30A.qfa3svRkRM0DJEOg0eoyhnkkZRc')
+bot.run('token')
